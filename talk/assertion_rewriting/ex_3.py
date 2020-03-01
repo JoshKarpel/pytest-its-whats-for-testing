c703339ccd17f35a0d1d1a670d827e46d665f98d
JoshKarpel <josh.karpel@gmail.com>
2020-03-01 14:05:04 -0600

    redo assertion rewriting "unwind" example

--- talk/assertion_rewriting/ex_3.py
+++ talk/assertion_rewriting/ex_3.py
@@ -1,9 +1,6 @@
 # pytest can "unwind" the call stack and tell you how a value came to exist
 
 
-def foo():
-    return 5
-
-
 def test_foo():
-    assert foo() == 4
+    with open('file.txt', mode = 'w') as f:
+        assert f.write('foo') > 0
